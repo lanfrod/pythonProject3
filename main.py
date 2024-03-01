38d1e50e202f6abc1d07834dbb3d18af7c44041c
lanfrod <cahbka.show123@gmail.com>
2024-03-01 21:11:01 +0300

    final dessision

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
-import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.uic import loadUi
+import sys, io
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
 import random
+
+
+temp = """<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>Form</class>
+ <widget class="QWidget" name="Form">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>500</width>
+    <height>600</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>Form</string>
+  </property>
+  <widget class="QPushButton" name="pushButton">
+   <property name="geometry">
+    <rect>
+     <x>170</x>
+     <y>540</y>
+     <width>141</width>
+     <height>41</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string>pushButton</string>
+   </property>
+  </widget>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
+"""
 
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        loadUi("UI.ui", self)
+        f = io.StringIO(temp)
+        uic.loadUi(f, self)
 
         self.scene = QtWidgets.QGraphicsScene()
         self.view = QtWidgets.QGraphicsView(self)
@@ -17,9 +52,10 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def add_circle(self):
         self.scene.clear()
-        diameter = random.randint(10, 400)
-        self.circle = QtWidgets.QGraphicsEllipseItem(-diameter / 2, -diameter / 2, diameter, diameter)
-        self.circle.setBrush(QtGui.QColor(255, 255, 0))
+        d = random.randint(10, 400)
+        n1, n2, n3 = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+        self.circle = QtWidgets.QGraphicsEllipseItem(-d / 2, -d / 2, d, d)
+        self.circle.setBrush(QtGui.QColor(n1, n2, n3))
         self.scene.addItem(self.circle)
 
 
